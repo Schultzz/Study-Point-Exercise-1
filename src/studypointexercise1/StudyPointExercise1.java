3fd38a9c237e2eda56e68d12d192829f6d736e33
Schultzz <ms.fuge@gmail.com>
2015-02-06 10:06:45 +0100

    Added Worlds simplest Web-Server

--- src/studypointexercise1/StudyPointExercise1.java
+++ src/studypointexercise1/StudyPointExercise1.java
@@ -9,7 +9,11 @@ import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.util.List;
@@ -31,6 +35,7 @@ public class StudyPointExercise1 {
         HttpServer server = HttpServer.create(new InetSocketAddress(ip, port), 0);
         server.createContext("/welcome", new RequestHandler());
         server.createContext("/headers", new HeaderHandler());
+        server.createContext("/pages", new SimpleFileHandler());
         server.setExecutor(null); // Use the default executor
         server.start();
         System.out.println("Server started, listening on port: " + port);
@@ -66,6 +71,26 @@ public class StudyPointExercise1 {
         }
     }
 
+      static class SimpleFileHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+            File file = new File(contentFolder + "index.html");
+            byte[] bytesToSend = new byte[(int) file.length()];
+            try {
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+                bis.read(bytesToSend, 0, bytesToSend.length);
+            } catch (IOException ie) {
+                System.out.println(ie);
+            }
+            he.sendResponseHeaders(200, bytesToSend.length);
+            try (OutputStream os = he.getResponseBody()) {
+                os.write(bytesToSend, 0, bytesToSend.length);
+            }
+           
+        }
+    }
+    
     static class HeaderHandler implements HttpHandler {
 
         @Override
